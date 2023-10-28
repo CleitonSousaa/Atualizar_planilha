ceab86e722bdd4b034af298ad9d0e92ae5bec73b
cleiton sousaa <fccleitonfs.329@gmail.com>
2023-10-28 13:22:50 -0300

    updating some things

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-# ler o arquivo a ser modificado
+# read the file to be modified
 tabela = pd.read_excel("produtos.xlsx")
 
-# variaveis
-imposto_maior = 1.6
-imposto_menor = 1.17
-desconto_mobilia = 0.07
-reajuste_games = 0.03
+# variables
+imposto_maior = 1.6 #60%
+imposto_menor = 1.17 #17%
+desconto_mobilia = 0.07 #7%
+reajuste_games = 0.03 #3$
 
 
-#TODOS OS PRODUTOS COM VALORES ACIMA DE 50$
+# ALL PRODUCTS WITH PRICES ABOVE 50$
 tabela.loc[tabela["PREÇO"] >= 50, "IMPOSTO SOB O PRODUTO"] = imposto_maior
 
 
-#TODOS OS PRODUTO COM VALORES INFERIORES A 50$
+# ALL PRODUCTS WITH PRICES LESS THAN 50$
 tabela.loc[tabela["PREÇO"] < 50, "IMPOSTO SOB O PRODUTO"] = imposto_menor
 
 
@@ -28,11 +28,12 @@ for indice, linha in tabela.iterrows():
         tabela.at[indice, "IMPOSTO SOB O PRODUTO"] = novo_valor
 
 
-# atualizacao do preco final
+# final price update
 tabela["PREÇO FINAL"] = tabela["IMPOSTO SOB O PRODUTO"] * tabela["PREÇO"] 
 
-# formatando preco final
+# formatting final price
 tabela["PREÇO FINAL"] = tabela["PREÇO FINAL"].round(2)
 
 
-tabela.to_excel("produto.xlsx", index=False)# cria novo arquivo alterado
+# creation of new changed file
+tabela.to_excel("produto.xlsx", index=False)
