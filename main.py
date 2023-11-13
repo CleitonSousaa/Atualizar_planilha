9893299a5305a74f597dc87888419ea44371d8e9
Francisco cleiton <117606967+CleitonSousaa@users.noreply.github.com>
2023-11-13 11:39:48 -0300

    Update main.py
    refatoração

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 import pandas as pd
 
-# read the file to be modified
 tabela = pd.read_excel("produtos.xlsx")
 
-# variables
-imposto_maior = 1.6 #60%
-imposto_menor = 1.17 #17%
-desconto_mobilia = 0.07 #-7%
-reajuste_games = 0.03 #+3%
+imposto_maior = 1.6 
+imposto_menor = 1.17 
+desconto_mobilia = 0.07 
+reajuste_games = 0.03 
 
-
-# ALL PRODUCTS WITH PRICES ABOVE 50$
 tabela.loc[tabela["PREÇO"] >= 50, "IMPOSTO SOB O PRODUTO"] = imposto_maior
-
-
-# ALL PRODUCTS WITH PRICES LESS THAN 50$
 tabela.loc[tabela["PREÇO"] < 50, "IMPOSTO SOB O PRODUTO"] = imposto_menor
-
 
 for indice, linha in tabela.iterrows():
     if linha["CATEGORIA"] == "mobilia":
         novo_valor = linha["IMPOSTO SOB O PRODUTO"] - desconto_mobilia
         tabela.at[indice, "IMPOSTO SOB O PRODUTO"] = novo_valor
-
     if linha["CATEGORIA"] == "games":
         novo_valor = linha["IMPOSTO SOB O PRODUTO"] + reajuste_games
         tabela.at[indice, "IMPOSTO SOB O PRODUTO"] = novo_valor
 
 
-# final price update
 tabela["PREÇO FINAL"] = tabela["IMPOSTO SOB O PRODUTO"] * tabela["PREÇO"] 
-
-# formatting final price
 tabela["PREÇO FINAL"] = tabela["PREÇO FINAL"].round(2)
 
-
-# creation of new changed file
 tabela.to_excel("produto.xlsx", index=False)
